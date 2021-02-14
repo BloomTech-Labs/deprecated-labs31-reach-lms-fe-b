30bf401c8dbb413afdb39b98c65eeeb1cff0dc36
kai-blt <blaitong@gmail.com>
2021-02-14 10:46:48 -0800

    adds basic homepage layout and navigation in the Home component. Tweaks layout styling and primary color in the theme-overrides.js

--- src/components/pages/home/RenderHomePage.jsx
+++ src/components/pages/home/RenderHomePage.jsx
@@ -1,34 +1,137 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from '../../common';
+import { Layout, Menu } from 'antd';
+import {
+  HomeOutlined,
+  LogoutOutlined,
+  SettingFilled,
+  FormOutlined,
+  EditOutlined,
+} from '@ant-design/icons';
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
+  const { Header, Content, Footer, Sider } = Layout;
+
+  const handleClick = e => {
+    //Checks on the Menu event.key and directs user to appropriate page
+    switch (e.key) {
+      case '1':
+        alert('Dashboard Clicked');
+        break;
+      case '2':
+        alert('Create Program Clicked');
+        break;
+      case '3':
+        alert('Modify Program Clicked');
+        break;
+      case '4':
+        alert('Settings Clicked');
+        break;
+      case '5':
+        authService.logout();
+        break;
+    }
+  };
+
   return (
-    <div>
-      <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
-      <div>
-        <p>
-          This is an example of a common example of how we'd like for you to
-          approach components.
-        </p>
-        <p>
-          <Link to="/profile-list">Profiles Example</Link>
-        </p>
-        <p>
-          <Link to="/example-list">Example List of Items</Link>
-        </p>
-        <p>
-          <Link to="/datavis">Data Visualizations Example</Link>
-        </p>
-        <p>
-          <Button
-            handleClick={() => authService.logout()}
-            buttonText="Logout"
-          />
-        </p>
-      </div>
-    </div>
+    <Layout>
+      {/*Header Area*/}
+      <Header
+        className="site-layout-sub-header-background"
+        style={{ padding: 0 }}
+      >
+        <div className="logo">
+          <h1>(LOGO) Reach LMS</h1>
+        </div>
+      </Header>
+
+      <Layout>
+        {/*Sidebar Navigation Container*/}
+        <Sider
+          breakpoint="sm"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          {/*Menu Items*/}
+          <Menu
+            theme="light"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            onClick={handleClick}
+          >
+            <Menu.Item key="1" icon={<HomeOutlined />}>
+              Dashboard
+            </Menu.Item>
+            <Menu.Item key="2" icon={<FormOutlined />}>
+              Create A Program
+            </Menu.Item>
+            <Menu.Item key="3" icon={<EditOutlined />}>
+              Modify Program
+            </Menu.Item>
+            <Menu.Item key="4" icon={<SettingFilled />}>
+              Settings
+            </Menu.Item>
+            <Menu.Item key="5" icon={<LogoutOutlined />}>
+              Logout
+            </Menu.Item>
+          </Menu>
+        </Sider>
+
+        {/*Main Content Area*/}
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            <h2>Welcome {userInfo.name}</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis
+              at consectetur lorem donec. Venenatis tellus in metus vulputate eu
+              scelerisque felis imperdiet. Sapien faucibus et molestie ac
+              feugiat sed. Eget aliquet nibh praesent tristique magna sit amet
+              purus. Integer quis auctor elit sed vulputate mi sit amet.
+              Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa.
+              Blandit cursus risus at ultrices mi tempus imperdiet nulla.
+              Pulvinar neque laoreet suspendisse interdum consectetur. Facilisis
+              sed odio morbi quis commodo odio aenean sed. Tincidunt nunc
+              pulvinar sapien et ligula ullamcorper malesuada. In dictum non
+              consectetur a erat. Amet volutpat consequat mauris nunc congue
+              nisi vitae. Maecenas ultricies mi eget mauris pharetra et ultrices
+              neque.Dictum non consectetur a erat. Et egestas quis ipsum
+              suspendisse ultrices gravida dictum. Aenean euismod elementum nisi
+              quis eleifend.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis
+              at consectetur lorem donec. Venenatis tellus in metus vulputate eu
+              scelerisque felis imperdiet. Sapien faucibus et molestie ac
+              feugiat sed. Eget aliquet nibh praesent tristique magna sit amet
+              purus. Integer quis auctor elit sed vulputate mi sit amet.
+              Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa.
+              Blandit cursus risus at ultrices mi tempus imperdiet nulla.
+              Pulvinar neque laoreet suspendisse interdum consectetur. Facilisis
+              sed odio morbi quis commodo odio aenean sed. Tincidunt nunc
+              pulvinar sapien et ligula ullamcorper malesuada. In dictum non
+              consectetur a erat. Amet volutpat consequat mauris nunc congue
+              nisi vitae. Maecenas ultricies mi eget mauris pharetra et ultrices
+              neque.Dictum non consectetur a erat. Et egestas quis ipsum
+              suspendisse ultrices gravida dictum. Aenean euismod elementum nisi
+              quis eleifend.
+            </p>
+          </div>
+        </Content>
+      </Layout>
+
+      {/*Footer*/}
+      <Footer style={{ textAlign: 'center' }}>Reach LMS ©2021</Footer>
+    </Layout>
   );
 }
 export default RenderHomePage;
--- src/styles/theme-overrides.js
+++ src/styles/theme-overrides.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '@primary-color': '#ec3944',
+  '@primary-color': '#0587A5',
   '@link-color': ' #ec3944',
   '@success-color': '#52c41a',
   '@warning-color': '#faad14',
@@ -13,4 +13,9 @@ module.exports = {
   '@border-color-base': '#d9d9d9',
   '@box-shadow-base':
     '0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)',
+  '@layout-body-background': '#fff',
+  '@layout-header-background': '#0587A5',
+  '@layout-trigger-background': 'red',
+  '@layout-trigger-color': '#0587A5',
+  '@layout-sider-background': '#fff',
 };
