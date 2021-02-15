1f7c7fd715bc5fb461ecf583fcd0a10847ed5fe3
kai-blt <blaitong@gmail.com>
2021-02-15 11:12:59 -0800

    adds styled-components and mock data for ModuleView component

--- src/components/pages/module-view/ModuleViewContainer.jsx
+++ src/components/pages/module-view/ModuleViewContainer.jsx
@@ -1,4 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+//Test Data
+const moduleData = {
+  moduleName: 'Test Module Name',
+  moduleTopics: [
+    {
+      moduleTopicName: 'Module Topic Name 1',
+      moduleTopicContent: 'Test Description 12345',
+    },
+    {
+      moduleTopicName: 'Module Topic Name 1',
+      moduleTopicContent: 'Test Description 12345',
+    },
+    {
+      moduleTopicName: 'Module Topic Name 1',
+      moduleTopicContent: 'Test Description 12345',
+    },
+  ],
+};
 
 const ModuleViewContainer = props => {
   return (
