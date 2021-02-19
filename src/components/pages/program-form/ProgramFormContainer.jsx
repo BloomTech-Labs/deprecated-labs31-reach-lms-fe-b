c1b62a4c9febddf64cb87dc2e66e44a52eef8d1d
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-02-19 17:28:30 -0500

    Finish ProgramForm and CRUD operations.

--- src/components/pages/program-form/ProgramFormContainer.jsx
+++ src/components/pages/program-form/ProgramFormContainer.jsx
@@ -8,50 +8,62 @@ import { programsActions } from '../../../state/ducks/programsDuck';
 import { userActions } from '../../../state/ducks/userDuck';
 
 export default props => {
+  /** The id of the program. Undefined if adding new program */
   const { id } = useParams();
   const dispatch = useDispatch();
+  /** if ID is defined, we'll populate this form with the
+   * contents of `program` as soon as `status === "success"` */
   const { program, status } = useSelector(state => state.programs);
+  /** The id of the current user (will be used for POST or PUT to verify admin user) */
   const userid = useSelector(state => state.user?.user?.userid);
-
+  /** AntD reusable form hook */
   const [form] = Form.useForm();
+  /** Determines whether the sub-form modal is visible. Initializes to false */
   const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
+    // if id is defined, then we are editing this program
+    // in that case, we should populate values with the existing program
     if (id) {
-      // if id is defined, then we are editing this program
-      // in that case, we should populate values with the existing program
       dispatch(programsActions.getProgramThunk(id));
     }
+
+    // no matter what (id or not), we have to get the user's information
+    // to POST or PUT by way of an id.
     dispatch(userActions.loginThunk());
   }, [id, dispatch]);
+
   useEffect(() => {
+    // if the status taken from state.program.status is "success",
+    // we know that the `getProgramThunk` successfully got a program
     if (status === 'success') {
+      // so we can set our form's values to match the program
       form.setFieldsValue({ ...program });
     }
   }, [status, form, program]);
 
-  const showClassModal = () => setModalVisible(true);
-  const hideClassModal = () => setModalVisible(false);
+  // just helper functions to show and hide the COURSE FORM modal
+  const showCourseModal = () => setModalVisible(true);
+  const hideCourseModal = () => setModalVisible(false);
 
   const onFinish = values => {
     if (id) {
-      console.log({ values });
-      dispatch(
-        programsActions.editProgramThunk({
-          ...values,
-          programId: id,
-          students: [],
-          teachers: [],
-          admin: { userid },
-        })
-      );
+      // if id is defined, we must hit the EDIT PROGRAM endpoint
+      const validEditedProgram = {
+        ...values,
+        programId: id, // the id of the program to update!
+        students: program.students || [], // stretch: could implement adding students
+        teachers: program.teachers || [], // stretch: could implement adding teachers
+        admin: { userid }, // this is currently required by backend to make sure user is admin
+      };
+      dispatch(programsActions.editProgramThunk(validEditedProgram));
     } else {
-      console.log({ ADD_SUBMIT: values });
+      // else we must just be creating a new program
       const validProgram = {
         ...values,
-        students: [],
-        teachers: [],
-        admin: { userid },
+        students: [], // stretch: could implement adding students
+        teachers: [], // stretch: could implement adding students
+        admin: { userid }, // this is currently required by backend to make sure user is admin
       };
       dispatch(programsActions.addProgramThunk(validProgram));
     }
@@ -68,6 +80,8 @@ export default props => {
   return (
     <Space direction="vertical" align="center" style={{ width: '100%' }}>
       <h1>Program Form</h1>
+
+      {/* PROGRAM ADD/EDIT FORM */}
       <Form
         form={form}
         name="programForm"
@@ -76,6 +90,7 @@ export default props => {
         autoComplete="off"
         layout="vertical"
       >
+        {/* PROGRAM NAME */}
         <Form.Item
           name="programName"
           label="Program Name"
@@ -83,6 +98,8 @@ export default props => {
         >
           <Input />
         </Form.Item>
+
+        {/* Program Type */}
         <Form.Item
           name="programType"
           label="Program Type"
@@ -95,10 +112,13 @@ export default props => {
             <Select.Option value="other">Other</Select.Option>
           </Select>
         </Form.Item>
+
+        {/* Program Description */}
         <Form.Item name="programDescription" label="Program Description">
           <Input.TextArea />
         </Form.Item>
 
+        {/* List of Course Cards for Each Course in This Program */}
         <Form.Item name="courses" label="Course List">
           {form.getFieldValue('courses')?.length > 0 ? (
             form
@@ -120,26 +140,31 @@ export default props => {
                 )
               )
           ) : (
+            // if no courses in program, display that to user
             <p>No courses yet!</p>
           )}
         </Form.Item>
+
+        {/* Add Class Button. On click will pull up ADD COURSE FORM (in a modal) */}
         <Form.Item>
-          <Button htmlType="button" onClick={showClassModal}>
-            Add Class
+          <Button htmlType="button" onClick={showCourseModal}>
+            Add Course
           </Button>
         </Form.Item>
 
+        {/* SUBMIT BUTTON */}
         <Form.Item>
           <Button htmlType="submit" type="primary">
             Submit
           </Button>
         </Form.Item>
       </Form>
+
+      {/* This modal will display if user clicks "ADD COURSE"  */}
       <Modal
         title="Course Modal"
         visible={modalVisible}
-        // onOk={onCourseAdd}
-        onCancel={hideClassModal}
+        onCancel={hideCourseModal}
       >
         <CourseForm isWrapped={true} onSubmit={onCourseAdd} />
       </Modal>
