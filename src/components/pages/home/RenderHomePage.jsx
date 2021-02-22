cd9f358a154fc2e43c5246f68ebbad7893d91a87
kai-blt <blaitong@gmail.com>
2021-02-22 09:27:18 -0800

    updates userDuck to remove console.log

--- src/components/pages/home/RenderHomePage.jsx
+++ src/components/pages/home/RenderHomePage.jsx
@@ -4,12 +4,10 @@ import { Layout } from 'antd';
 import { DashboardView } from '../dashboard-view';
 import { useDispatch } from 'react-redux';
 import { userActions } from '../../../state/ducks/userDuck';
-import styled from 'styled-components';
 
 /* cSpell:disable */
 function RenderHomePage(props) {
   const { authService } = props;
-  const { Content } = Layout;
   const dispatch = useDispatch();
 
   useEffect(() => {
