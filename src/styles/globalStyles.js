27f824f8d89b9f450e1d8ea7e83f3b7411cb51c0
Christopher Girvin <chrisg8691@gmail.com>
2021-02-23 11:25:09 -0500

    made requested changes
    removed ant-overrides files and moved contents to globalStyles as well as removed the import of overrride file into index.

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -14,6 +14,31 @@ const GlobalStyle = createGlobalStyle`
     h1, h2, h3, h4, h5, h6 {
         font-weight: 700;
     }
+    @media (max-width: 575px) {
+      .ant-layout-sider-dark{
+        position: absolute;
+        z-index: 3;
+        width: 100vw;
+        right: 0;
+        left: 0;
+        height: 100%;
+      }
+    }
+    /*
+        Remove middle divider on cards for dashboard
+        view between main card body and buttons
+     */
+    .ant-card-actions {
+      border-top: none;
+    }
+
+    /*
+        Remove dividers between buttons for cards
+        on main dashboard view
+     */
+    .ant-card-actions > li:not(:last-child) {
+      border-right: none;
+    }
 
     //Mobile devices
     @media(min-width: 320px) {
